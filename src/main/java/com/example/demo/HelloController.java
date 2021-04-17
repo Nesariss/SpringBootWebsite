8ed73c7f82cea131f256cfa778b9b7cb41bbcac4
Nesariss <marcinkordowski10@wp.pl>
2021-04-17 16:22:03 +0200

    make it better

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HelloController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static String modell;
+
     @GetMapping("/car")
     public String get(Model model){
         Car car= new Car("mazda", "3");
@@ -20,9 +27,14 @@ public class HelloController {
     @PostMapping("/add")
     public String addCar(@ModelAttribute Car car){
         System.out.println(car.getMark());
-
-
-
+        modell=car.getMark();
+        int id=16;
+        int wiek=22;
+        String sql="INSERT INTO uzytkownik (id,wiek,model) VALUES(?,?,?)";
+        int rows=jdbcTemplate.update(sql,id,wiek,modell);
+        if(rows>0){
+            System.out.println("A new row has been added");
+        }
         return "redirect:/car";
     }
 }
