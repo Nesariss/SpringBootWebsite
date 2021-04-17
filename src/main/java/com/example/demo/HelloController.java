9069be4faa8c5cdef26bb3d8b2d7affa500d7326
Nesariss <marcinkordowski10@wp.pl>
2021-04-17 16:12:17 +0200

    make it better

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,12 +3,26 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/car")
     public String get(Model model){
+        Car car= new Car("mazda", "3");
         model.addAttribute("name", "Przemek");
-        return"hello";
+        model.addAttribute("car", car);
+        model.addAttribute("newCar",new Car());
+        return"car";
+    }
+
+    @PostMapping("/add")
+    public String addCar(@ModelAttribute Car car){
+        System.out.println(car.getMark());
+
+
+
+        return "redirect:/car";
     }
 }
